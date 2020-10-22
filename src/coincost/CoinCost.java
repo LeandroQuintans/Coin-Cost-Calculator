359cc91db90a9171fce9335d44c8368083d0c7f3
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2020-10-22 19:24:30 +0100

    added main for manual testing

--- src/coincost/CoinCost.java
+++ src/coincost/CoinCost.java
@@ -1,7 +1,11 @@
 package coincost;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CoinCost {
     private CoinStockpile[] wallet;
@@ -37,7 +41,35 @@ public class CoinCost {
 
         return sBuilder.toString();
     }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] coinStockpileStrings = sc.nextLine().split(" ");
+        BigDecimal cost = new BigDecimal(sc.next());
+        sc.close();
         
+        Pattern pattern = Pattern.compile("\\((\\d+.\\d+|\\d+),(\\d+)\\)");
+        
+        CoinStockpile[] coinStockpiles = new CoinStockpile[coinStockpileStrings.length];
+        int i = 0;
+        for (String coinStockpileString : coinStockpileStrings) {
+            Matcher matcher = pattern.matcher(coinStockpileString);
+            matcher.matches();
+            String value = matcher.group(1);
+            int amount = Integer.parseInt(matcher.group(2));
+
+            coinStockpiles[i++] = new CoinStockpile(value, amount);
+        }
+
+        CoinCost coinCost = new CoinCost(coinStockpiles, cost);
+
+        Iterator<CoinStockpile[]> payments = coinCost.payments();
+
+        while (payments.hasNext()) {
+            for (CoinStockpile coinStockpile : payments.next()) {
+                System.out.print(coinStockpile.toString() + ' ');
+            }
+            System.out.println();
+        }
     }
 }
