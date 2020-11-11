f334f5cd37b80bfb86c5269011fef9bc3ec8dcba
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2020-11-11 21:33:56 +0000

    big refactor

--- src/coincost/TopDownPaymentCalc.java
+++ src/coincost/TopDownPaymentCalc.java
@@ -2,20 +2,21 @@ package coincost;
 
 import java.math.BigDecimal;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
-public class TopDownPaymentCalc implements Iterator<CoinPile>, IPaymentCalc {
+public class TopDownPaymentCalc implements IPaymentCalc {
     private CoinCost cc;
     private CoinPile unusedCoinPile;
-    private Set<BigDecimal> cpKeySet;
     private CoinPile currentCoinPile;
+    private List<CoinPile> payments;
 
     public TopDownPaymentCalc(CoinCost cc) {
         this.cc = cc;
         unusedCoinPile = new CoinPile(cc.getWallet());
-        cpKeySet = unusedCoinPile.keySet();
         currentCoinPile = new CoinPile();
-        start(cpKeySet.iterator());
+        payments = new LinkedList<>();
     }
 
     public void start(Iterator<BigDecimal> itr) {
@@ -30,24 +31,23 @@ public class TopDownPaymentCalc implements Iterator<CoinPile>, IPaymentCalc {
             }
             currentCoinPile.addAmount(key, amount);
         } while (currentCoinPile.getTotal().compareTo(cc.getCost()) < 0 && itr.hasNext());
+
+        // return currentCoinPile;
+    }
+
+    public void prepare(Iterator<BigDecimal> itr) {
+        
     }
 
     @Override
-    public boolean hasNext() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+    public List<CoinPile> payments() {
+        start(unusedCoinPile.keySet().iterator());
+        payments.add(new CoinPile(currentCoinPile));
 
-    @Override
-    public CoinPile next() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+        // while (next(cpKeySet.iterator()))
 
-    @Override
-    public Iterator<CoinPile> payments() {
-        // TODO Auto-generated method stub
-        return null;
+
+        return payments;
     }
     
 }
