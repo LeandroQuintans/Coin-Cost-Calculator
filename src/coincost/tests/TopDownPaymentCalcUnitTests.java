aaddc6522510c91613d9942177dcc0c20d918573
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2020-11-12 02:38:15 +0000

    payment test

--- src/coincost/tests/TopDownPaymentCalcUnitTests.java
+++ src/coincost/tests/TopDownPaymentCalcUnitTests.java
@@ -3,6 +3,7 @@ package coincost.tests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,7 @@ public class TopDownPaymentCalcUnitTests {
     private CoinCost cc = new CoinCost(
         new CoinPile(Map.of(
             new BigDecimal("1.00"), 1,
-            new BigDecimal("0.50"), 2,
+            new BigDecimal("0.50"), 5,
             new BigDecimal("0.20"), 2,
             new BigDecimal("0.10"), 3,
             new BigDecimal("0.05"), 2,
@@ -31,9 +32,7 @@ public class TopDownPaymentCalcUnitTests {
     //     CoinPile expected = new CoinPile(
     //         Map.of(
     //             new BigDecimal("1.00"), 1,
-    //             new BigDecimal("0.50"), 2,
-    //             new BigDecimal("0.20"), 2,
-    //             new BigDecimal("0.10"), 1,
+    //             new BigDecimal("0.50"), 3,
     //             new BigDecimal("0.05"), 1,
     //             new BigDecimal("0.02"), 2
     //         )
@@ -43,5 +42,58 @@ public class TopDownPaymentCalcUnitTests {
 
     //     assertEquals(expected.toString(), actual.toString());
     // }
+
+    @Test
+    public void paymentsTest() {
+        List<CoinPile> expected = List.of(
+            new CoinPile(
+                Map.of(
+                    new BigDecimal("1.00"), 1,
+                    new BigDecimal("0.50"), 3,
+                    new BigDecimal("0.05"), 1,
+                    new BigDecimal("0.02"), 2
+                )
+            ),
+            new CoinPile(
+                Map.of(
+                    new BigDecimal("0.50"), 5,
+                    new BigDecimal("0.05"), 1,
+                    new BigDecimal("0.02"), 2
+                )
+            ),
+            new CoinPile(
+                Map.of(
+                    new BigDecimal("0.50"), 4,
+                    new BigDecimal("0.20"), 2,
+                    new BigDecimal("0.10"), 1,
+                    new BigDecimal("0.05"), 1,
+                    new BigDecimal("0.02"), 2
+                )
+            ),
+            new CoinPile(
+                Map.of(
+                    new BigDecimal("0.50"), 4,
+                    new BigDecimal("0.20"), 1,
+                    new BigDecimal("0.10"), 3,
+                    new BigDecimal("0.05"), 1,
+                    new BigDecimal("0.02"), 2
+                )
+            ),
+            new CoinPile(
+                Map.of(
+                    new BigDecimal("0.50"), 4,
+                    new BigDecimal("0.20"), 1,
+                    new BigDecimal("0.10"), 2,
+                    new BigDecimal("0.05"), 2,
+                    new BigDecimal("0.02"), 4,
+                    new BigDecimal("0.01"), 1
+                )
+            )
+        );
+
+        List<CoinPile> actual = tdpc.payments();
+
+        assertEquals(expected.toString(), actual.toString());
+    }
 
 }
