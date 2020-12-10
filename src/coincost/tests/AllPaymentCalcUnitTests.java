01d8f997c3d5f05f60a6ec81183ab8775d62b4b6
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2020-12-10 16:23:11 +0000

    added some AllPaymentCalc tests

--- src/coincost/tests/AllPaymentCalcUnitTests.java
+++ src/coincost/tests/AllPaymentCalcUnitTests.java
@@ -1,6 +1,7 @@
 package coincost.tests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
 import java.util.Map;
@@ -14,18 +15,19 @@ import coincost.Wallet;
 
 
 public class AllPaymentCalcUnitTests {
-    private CoinCost cc = new CoinCost(
-        new Wallet(Map.of(
-            new BigDecimal("1.00"), 2,
-            new BigDecimal("0.50"), 4,
-            new BigDecimal("0.10"), 6
-        )), 
-        new BigDecimal("2.60")
-    );
-    private AllPaymentCalc apc = new AllPaymentCalc(cc);
 
     @Test
-    public void paymentsTest() {
+    public void paymentsTest1() {
+        CoinCost cc = new CoinCost(
+            new Wallet(Map.of(
+                new BigDecimal("1.00"), 2,
+                new BigDecimal("0.50"), 4,
+                new BigDecimal("0.10"), 6
+            )), 
+            new BigDecimal("2.60")
+        );
+        AllPaymentCalc apc = new AllPaymentCalc(cc);
+
         Set<Wallet> expected = Set.of(
             new Wallet(
                 Map.of(
@@ -67,6 +69,78 @@ public class AllPaymentCalcUnitTests {
         Set<Wallet> actual = apc.payments();
 
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void paymentsTest2() {
+        CoinCost cc = new CoinCost(
+            new Wallet(Map.of(
+                new BigDecimal("1.00"), 2,
+                new BigDecimal("0.50"), 4,
+                new BigDecimal("0.10"), 6
+            )), 
+            new BigDecimal("2.59")
+        );
+        AllPaymentCalc apc = new AllPaymentCalc(cc);
+
+        Set<Wallet> expected = Set.of(
+            new Wallet(
+                Map.of(
+                    new BigDecimal("1.00"), 2,
+                    new BigDecimal("0.50"), 1,
+                    new BigDecimal("0.10"), 1
+                )
+            ),
+            new Wallet(
+                Map.of(
+                    new BigDecimal("1.00"), 2,
+                    new BigDecimal("0.50"), 0,
+                    new BigDecimal("0.10"), 6
+                )
+            ),
+            new Wallet(
+                Map.of(
+                    new BigDecimal("1.00"), 1,
+                    new BigDecimal("0.50"), 3,
+                    new BigDecimal("0.10"), 1
+                )
+            ),
+            new Wallet(
+                Map.of(
+                    new BigDecimal("1.00"), 1,
+                    new BigDecimal("0.50"), 2,
+                    new BigDecimal("0.10"), 6
+                )
+            ),
+            new Wallet(
+                Map.of(
+                    new BigDecimal("1.00"), 0,
+                    new BigDecimal("0.50"), 4,
+                    new BigDecimal("0.10"), 6
+                )
+            )
+        );
+
+        Set<Wallet> actual = apc.payments();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void paymentsTest3() {
+        CoinCost cc = new CoinCost(
+            new Wallet(Map.of(
+                new BigDecimal("1.00"), 2,
+                new BigDecimal("0.50"), 4,
+                new BigDecimal("0.10"), 6
+            )), 
+            new BigDecimal("4.61")
+        );
+        AllPaymentCalc apc = new AllPaymentCalc(cc);
+
+        Set<Wallet> actual = apc.payments();
+
+        assertTrue(actual.isEmpty());
     }
 
 }
