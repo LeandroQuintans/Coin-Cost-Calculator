bd29adf9e2910b8f8c8917ad73b57f149624a858
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2021-03-02 03:43:17 +0000

    IPaymentCalc documentation

--- src/coincost/IPaymentCalc.java
+++ src/coincost/IPaymentCalc.java
@@ -2,6 +2,16 @@ package coincost;
 
 import java.util.Set;
 
+/**
+ * <p>Interface that exposes the payments method</p>
+ * <p>This interface should be used for different 
+ * algorithms to calculate the payments of a CoinCost entity</p>
+ * @author Leandro Quintans
+ */
 public interface IPaymentCalc {
+    /**
+     * <p>Calculates all the payments and returns a set of them</p>
+     * @return Set of payments
+     */
     public Set<Wallet> payments();
 }
