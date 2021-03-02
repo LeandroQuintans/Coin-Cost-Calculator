041dd83dc8e8843796511d06b3c4312a40c04343
Leandro Quintans <44684615+LeandroQuintans@users.noreply.github.com>
2021-03-02 16:24:20 +0000

    Documentation and added constructors

--- src/coincost/exceptions/NegativeCoinAmountException.java
+++ src/coincost/exceptions/NegativeCoinAmountException.java
@@ -1,10 +1,20 @@
 package coincost.exceptions;
 
+/**
+ * <p>RuntimeException used when there is an attempt to input or 
+ * decrease a coin/bill's amount in the wallet down to negative values.</p>
+ * @author Leandro Quintans
+ */
 public class NegativeCoinAmountException extends RuntimeException {
 
-    /**
-     *
-     */
     private static final long serialVersionUID = 4278817640583465485L;
+
+    public NegativeCoinAmountException() {
+        super();
+    }
+
+    public NegativeCoinAmountException(String message) {
+        super(message);
+    }
 
 }
